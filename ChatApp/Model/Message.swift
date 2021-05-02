6e3654de165ca003c67c3fd4bf8401fc5834ad08
Ryoh <Ryoh.popy@gmail.com>
2021-05-03 08:01:26 +0900

    [add] メッセージ構造体作成

---
+++ ChatApp/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by Ryoh on 2021/05/03.
+//
+
+import Foundation
+
+struct Message{
+    let sender:String
+    let body:String
+    let imageString:String
+}
