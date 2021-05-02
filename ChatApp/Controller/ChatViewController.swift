9a8233bc0c157edfc4ac0d0beeee15940e538e02
Ryoh <Ryoh.popy@gmail.com>
2021-05-03 08:26:00 +0900

    [add] メッセージ受信メソッド追加

--- ChatApp/Controller/ChatViewController.swift
+++ ChatApp/Controller/ChatViewController.swift
@@ -12,7 +12,7 @@ import SDWebImage
 class ChatViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
     
     
-
+    
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,6 +21,9 @@ class ChatViewController: UIViewController ,UITableViewDelegate,UITableViewDataS
     
     var roomName = String()
     var imageString = String()
+    
+    // メッセージ構造体が入る配列
+    var messages:[Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,9 +41,47 @@ class ChatViewController: UIViewController ,UITableViewDelegate,UITableViewDataS
         }
         
         self.navigationItem.title = roomName
-
         // Do any additional setup after loading the view.
     }
+    
+    
+    func loadMessages(roomName:String){
+        // 日付順に取得
+        db.collection(roomName).order(by: "date").addSnapshotListener{ (snapShot,error) in
+            self.messages = []
+            
+            if error != nil{
+                print(error.debugDescription)
+                return
+            }
+            if let snapShotDoc = snapShot?.documents{
+                
+                for doc in snapShotDoc{
+                    let date = doc.data()
+                    if let sender = date["sender"] as? String,let
+                        body = date["body"] as? String,let
+                            imageString = date["imageString"] as? String{
+                        
+                        let newMessage = Message(sender: sender, body: body, imageString: imageString)
+                        
+                        self.messages.append(newMessage)
+                        
+                        DispatchQueue.main.async {
+                            // リロードしてテーブルにメッセージを読み込む
+                            self.tableView.reloadData()
+                            
+                            let indexPath = IndexPath(row:
+                                                        self.messages.count - 1, section:0)
+                            self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         <#code#>
     }
@@ -66,22 +107,22 @@ class ChatViewController: UIViewController ,UITableViewDelegate,UITableViewDataS
                     //キーボード閉じる
                     self.messageTextField.resignFirstResponder()
                 }
-               
+                
                 
                 
             }
         }
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
