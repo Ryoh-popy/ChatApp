638fe28bbff0a9efa8e3b8e6e64cbf6cede35cbc
Ryoh <Ryoh.popy@gmail.com>
2021-05-03 11:33:13 +0900

    [add] チャットルーム作成処理

---
+++ ChatApp/Model/RoomModel.swift
@@ -0,0 +1,14 @@
+//
+//  RoomModel.swift
+//  ChatApp
+//
+//  Created by Ryoh on 2021/05/03.
+//
+
+import Foundation
+
+struct RoomModel{
+    
+    var roomName = String()
+    var roomImageString = String()
+}
